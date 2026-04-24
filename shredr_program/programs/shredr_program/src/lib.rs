3199f517386c3fe8bdd1e6adc42e31b2dbeecf98
Toastx <toastx.sol@gmail.com>
2026-04-24 18:47:40 +0530

    fix: add lib.rs

--- shredr_program/programs/shredr_program/src/lib.rs
+++ shredr_program/programs/shredr_program/src/lib.rs
@@ -1,8 +1,12 @@
 use anchor_lang::prelude::*;
+use ephemeral_rollups_sdk::ephemeral;
+
 pub mod instructions;
 pub mod state;
-use crate::instructions::*;
-use crate::state::*;
+
+use instructions::*;
+use state::*;
+
 declare_id!("A6aPwpTxFhQ9oSxdu1W8AH97LqjFwACXW81RkuTNAVEC");
 
 #[ephemeral]
@@ -10,11 +14,19 @@ declare_id!("A6aPwpTxFhQ9oSxdu1W8AH97LqjFwACXW81RkuTNAVEC");
 pub mod shredr_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize_and_delegate(
+        ctx: Context<InitializeAndDelegate>,
+        salt: [u8; 32],
+        burner_pubkey: Pubkey,
+    ) -> Result<()> {
+        instructions::initialize_and_delegate(ctx, salt, burner_pubkey)
+    }
+
+    pub fn private_transfer(ctx: Context<PrivateTransfer>, amount: u64) -> Result<()> {
+        instructions::private_transfer(ctx, amount)
+    }
+
+    pub fn settle_and_undelegate(ctx: Context<SettleAndUndelegate>) -> Result<()> {
+        instructions::settle_and_undelegate(ctx)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
