bb900ebc9221d3877ce7cdcc0ab85ebe72d33424
toastx <toastx.sol@gmail.com>
2026-04-26 22:01:04 +0530

    init: setup pinocchio dev and test program

---
+++ shredr-program/src/lib.rs
@@ -0,0 +1,30 @@
+// lib.rs
+#![no_std]
+#![allow(unexpected_cfgs)]
+
+use pinocchio::{AccountView,
+    entrypoint,
+    error::ProgramError,
+    Address,
+    ProgramResult,
+};
+use pinocchio_pubkey::declare_id;
+entrypoint!(process_instruction);
+pub mod instructions;
+pub mod helpers;
+pub mod constants;
+use crate::instructions::instructions::{Deposit,Withdraw};
+
+declare_id!("H64YCQTWdQkx9vjs1ZB2Uo24FyUBibnDxhKdznamybpZ");
+
+fn process_instruction(
+    _program_id: &Address,
+    accounts: & mut [AccountView],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    match instruction_data.split_first() {
+        Some((Deposit::DISCRIMINATOR, data)) => Deposit::try_from((data, accounts))?.process(),
+        Some((Withdraw::DISCRIMINATOR, _)) => Withdraw::try_from(accounts)?.process(),
+        _ => Err(ProgramError::InvalidInstructionData),
+    }
+}
