11d0be936abe92d677823ea3417f332846ba7a6b
Toastx <toastx.sol@gmail.com>
2026-04-27 19:32:24 +0530

    feat: partial impl of initialise and delegate

--- shredr-program/src/constants.rs
+++ shredr-program/src/constants.rs
@@ -1,3 +1,8 @@
 use pinocchio::Address;
 
 pub const PROGRAM_ADDRESS:Address = Address::new_from_array(crate::ID);
+pub mod seeds {
+    pub const PROGRAM_CONFIG:&[u8] = b"shredr_program_config";
+    pub const STEALTH_ADDRESS:&[u8] = b"shredr_stealth_address";
+    pub const USER_ADDRESS:&[u8] = b"shredr_user_address";
+}
