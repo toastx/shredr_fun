c917fb09295bf8ecb2a28140308c6f0cecaa1686
Toastx <toastx.sol@gmail.com>
2026-05-06 18:28:10 +0530

    fix: remove dead code

--- shredr-program/src/lib.rs
+++ shredr-program/src/lib.rs
@@ -14,18 +14,42 @@ pub mod instructions;
 pub mod helpers;
 pub mod constants;
 pub mod state;
-use crate::instructions::instructions::{Deposit,Withdraw};
+
+use crate::instructions::initialize_delegate::InitializeAndDelegate;
+use crate::instructions::private_transfer::PrivateTransfer;
+use crate::instructions::commit_undelegate::{CommitStealth, CommitAndUndelegateStealth, UndelegationCallback};
+use crate::instructions::withdraw::Withdraw;
 
 declare_id!("H64YCQTWdQkx9vjs1ZB2Uo24FyUBibnDxhKdznamybpZ");
 
 fn process_instruction(
-    _program_id: &Address,
-    accounts: & mut [AccountView],
+    program_id: &Address,
+    accounts: &[AccountView],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    match instruction_data.split_first() {
-        Some((Deposit::DISCRIMINATOR, data)) => Deposit::try_from((data, accounts))?.process(),
-        Some((Withdraw::DISCRIMINATOR, _)) => Withdraw::try_from(accounts)?.process(),
+    let (discriminator, data) = instruction_data
+        .split_first()
+        .ok_or(ProgramError::InvalidInstructionData)?;
+
+    match discriminator {
+        &InitializeAndDelegate::DISCRIMINATOR => {
+            InitializeAndDelegate::try_from((accounts, data))?.process()
+        }
+        &PrivateTransfer::DISCRIMINATOR => {
+            PrivateTransfer::try_from((data, accounts))?.process()
+        }
+        &CommitStealth::DISCRIMINATOR => {
+            CommitStealth::try_from((accounts, data))?.process()
+        }
+        &CommitAndUndelegateStealth::DISCRIMINATOR => {
+            CommitAndUndelegateStealth::try_from((accounts, data))?.process()
+        }
+        &Withdraw::DISCRIMINATOR => {
+            Withdraw::try_from((accounts, data))?.process()
+        }
+        &UndelegationCallback::DISCRIMINATOR => {
+            UndelegationCallback::try_from((accounts, data))?.process(program_id)
+        }
         _ => Err(ProgramError::InvalidInstructionData),
     }
 }
