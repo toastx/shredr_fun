2e98d069ea408a806c1e311dee49e434e1541358
toastx <toastx.sol@gmail.com>
2026-08-25 01:42:35 +0530

    chore: remove unused components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,7 @@
 // Barrel exports for all components
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
-export { default as GeneratorCard } from './GeneratorCard';
 export { default as AddressDisplay } from './AddressDisplay';
-export { default as SeedDisplay } from './SeedDisplay';
-export { default as HashDisplay } from './HashDisplay';
 export { TransactionMonitor } from './TransactionMonitor';
 export { TransactionApprovalModal } from './TransactionApprovalModal';
 export { WalletButton } from './WalletButton';
